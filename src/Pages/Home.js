98ba378c8503eabd4bd5959ac76cb76f71b3098e
Justin Swift <jlswift2@gmail.com>
2022-05-25 23:53:47 -0500

    Add small intro paragraph

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Hello, my name is Justin Swift</h1>
+        <h2>I am a full-stack software developer with experience in Javascript, React.js, Ruby on Rails, and more!</h2>
+    </div>
   )
 }
 
