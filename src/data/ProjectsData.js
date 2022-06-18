614b9e9294b2b3fdcfaf3f0dc356a93479d408e6
Justin Swift <jlswift2@gmail.com>
2022-06-17 22:34:19 -0500

    Populate projects data

---
+++ src/data/ProjectsData.js
@@ -0,0 +1,63 @@
+export const projects = [
+    {
+        name: "Build Your Own Book",
+        summary: "A full-stack recipe storing and sharing application equipped with highly customizable input forms, robust search tools, and secure account encryption.",
+        img: "",
+        languages: [
+            "Ruby on Rails",
+            "ReactJS",
+            "PostgreSQL",
+            "AWS",
+            "Tailwind"
+        ],
+        demo: "https://www.youtube.com/watch?v=TKH3xrsJFnM",
+        deployed: "",
+        github: "https://github.com/jlswift2/swift-final-project"
+    },
+    {
+        name: "Postcard Generator",
+        summary: "Postcard Generator is a free service dedicated to providing individuals in hospitals or assisted-living facilities with uplifting cards from caring individuals across the globe.",
+        img: "",
+        languages: [
+            "Ruby on Rails",
+            "ReactJS",
+            "PostgreSQL",
+            "AWS",
+            "Fabric.js",
+            "Tailwind",
+            "Heroku"
+        ],
+        demo: "",
+        deployed: "https://postcard-generator.herokuapp.com/",
+        github: "https://github.com/b-vu/postcard-generator"
+    },
+    {
+        name: "BITE",
+        summary: "A user-friendly restaurant menu for business owners",
+        img: "",
+        languages: [
+            "Ruby",
+            "Sinatra",
+            "ReactJS",
+            "SQLite",
+            "Tailwind"
+        ],
+        demo: "",
+        deployed: "",
+        github: "https://github.com/jlswift2/phase-3-restaurant-app-frontend"
+    },
+    {
+        name: "Bubbles",
+        summary: "A social experience to share your thoughts",
+        img: "",
+        languages: [
+            "ReactJS",
+            "CSS",
+            "Heroku"
+        ],
+        demo: "",
+        deployed: "https://guarded-hollows-05759.herokuapp.com/",
+        github: "https://github.com/jlswift2/phase-2-project/tree/main/src/components"
+    },
+
+]
