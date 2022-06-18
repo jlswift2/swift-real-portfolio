5a16353b24e32df799b7e2c9a221be0c0e98839e
Justin Swift <jlswift2@gmail.com>
2022-06-17 21:12:08 -0500

    Create basic components

---
+++ src/Components/AboutSnippet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutSnippet() {
+  return (
+    <div>AboutSnippet</div>
+  )
+}
+
+export default AboutSnippet
---
+++ src/Components/HomeIntro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomeIntro() {
+  return (
+    <div>
+        <h1>Hello, my name is Justin Swift</h1>
+        <h2>I am a full-stack software developer with experience in React JS, Ruby on Rails, and more!</h2>
+    </div>
+  )
+}
+
+export default HomeIntro
