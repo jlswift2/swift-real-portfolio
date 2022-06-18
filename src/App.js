53547104b46b279c3624f2a5f0dc55bade443d20
Justin Swift <jlswift2@gmail.com>
2022-06-17 21:12:36 -0500

    Add About path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Home from './Pages/Home';
 import Resume from './Pages/Resume';
+import About from './Pages/About'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/"> 
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
           <Route path='resume' element={<Resume />} />
         </Route>
       </Routes>
