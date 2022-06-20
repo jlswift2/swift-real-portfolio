7a94dc7953bf7a1a3306d7eea78613b72daf5786
Justin Swift <jlswift2@gmail.com>
2022-06-19 23:24:11 -0500

    Render individual projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {projects} from "../data/ProjectsData"
+import Project from './Project'
 
 function Projects() {
+  let projectElements = projects.map(proj => {
+    return <Project proj={proj} />
+  })
+
   return (
-    <div>Projects</div>
+    <div>
+      <h2>Projects</h2>
+      {projectElements}
+    </div>
   )
 }
 
