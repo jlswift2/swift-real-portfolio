f7a019782b8356eada7cfc650ed9e7eb775f231e
Justin Swift <jlswift2@gmail.com>
2022-06-21 21:53:56 -0500

    Add About Me snippet and redirect button

--- src/components/AboutSnippet.js
+++ src/components/AboutSnippet.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function AboutSnippet() {
+  let navigate = useNavigate()
+
   return (
-    <div>AboutSnippet</div>
+    <div>
+      <h2>About Me</h2>
+      <h4>
+        I am a seasoned professional seeking to leverage my creative ability, problem-solving skills, 
+        and technical know-how in the software development sector. With full-stack experience using Ruby on Rails and React, it is my goal to provide effective and efficient product that is easy to read and reuse by other developers. Perhaps most importantly, I also bake a mean cookie.
+      </h4>
+      <button onClick={() => navigate("about")}>More about me...</button>
+    </div>
   )
 }
 
